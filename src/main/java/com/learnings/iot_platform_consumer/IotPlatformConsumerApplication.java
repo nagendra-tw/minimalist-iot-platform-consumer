d464ef09a7f071c3847bc7e38b04d0f8c3b0ad0b
Nagendra Babu <nagendrababu.marasu@thoughtworks.com>
2024-12-09 10:19:26 +0530

    Add. project initial setup

---
+++ src/main/java/com/learnings/iot_platform_consumer/IotPlatformConsumerApplication.java
@@ -0,0 +1,13 @@
+package com.learnings.iot_platform_consumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IotPlatformConsumerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IotPlatformConsumerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/learnings/iot_platform_consumer/IotPlatformConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learnings.iot_platform_consumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IotPlatformConsumerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
