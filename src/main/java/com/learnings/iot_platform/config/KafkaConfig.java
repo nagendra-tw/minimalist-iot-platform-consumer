e3c7510e239a5f2aa1fe33866bd07dcada1e8603
Nagendra Babu <nagendrababu.marasu@thoughtworks.com>
2024-12-10 12:21:43 +0530

    Add. kafka configuration to create alert topic

--- src/main/java/com/learnings/iot_platform/config/KafkaConfig.java
+++ src/main/java/com/learnings/iot_platform/config/KafkaConfig.java
@@ -1,25 +1,33 @@
 package com.learnings.iot_platform.config;
 
+import com.learnings.iot_platform.constants.Constants;
+import org.apache.kafka.clients.admin.NewTopic;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.StringDeserializer;
+import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
-import org.springframework.kafka.core.ConsumerFactory;
-import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+import org.springframework.kafka.config.TopicBuilder;
+import org.springframework.kafka.core.*;
 import org.springframework.kafka.listener.DefaultErrorHandler;
 import org.springframework.kafka.support.serializer.ErrorHandlingDeserializer;
 import org.springframework.kafka.support.serializer.JsonDeserializer;
+import org.springframework.kafka.support.serializer.JsonSerializer;
 
 import java.util.HashMap;
 import java.util.Map;
 
 @Configuration
 public class KafkaConfig {
-    @Autowired
-    private Environment env;
+    private final Environment env;
+
+    public KafkaConfig(Environment env) {
+        this.env = env;
+    }
 
     @Bean
     public ConsumerFactory<String, Object> consumerFactory() {
@@ -44,5 +52,29 @@ public class KafkaConfig {
         factory.setConsumerFactory(consumerFactory());
         factory.setCommonErrorHandler(errorHandler);
         return factory;
+    }
+
+    @Bean
+    public ProducerFactory<String, Object> producerFactory() {
+        Map<String, Object> config = new HashMap<>();
+        config.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, env.getProperty("spring.kafka.consumer.bootstrap-servers"));
+        config.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        config.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
+        return new DefaultKafkaProducerFactory<>(config);
+    }
+
+    @Bean
+    public KafkaTemplate<String, Object> kafkaTemplate(ProducerFactory<String, Object> producerFactory) {
+        return new KafkaTemplate<>(producerFactory);
+    }
+
+    @Bean
+    NewTopic createSensorAlertGeneratedEventTopic() {
+        System.out.println("Create Sensor Alert Generated Event Topic");
+        return TopicBuilder
+                .name(Constants.SENSOR_ALERT_GENERATED_EVENT_TOPIC)
+                .partitions(3)
+                .replicas(1)
+                .build();
     }
 }
--- src/main/java/com/learnings/iot_platform/constants/Constants.java
+++ src/main/java/com/learnings/iot_platform/constants/Constants.java
@@ -2,6 +2,7 @@ package com.learnings.iot_platform.constants;
 
 public class Constants {
     public static final String SENSOR_DATA_STORED_EVENT_TOPIC="sensor-data-stored-events-topic";
+    public static final String SENSOR_ALERT_GENERATED_EVENT_TOPIC="sensor-alert-generated-events-topic";
     public static final double AVERAGE_TEMPERATURE_THRESHOLD = 40.0;
     public static final double LOW_BATTERY_THRESHOLD = 20.0;
 
